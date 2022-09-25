0e6d7f3cded15390dc4476a195a6f34cd64e74fc
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-09-24 22:48:23 -0400

    HW3 Updated

--- solution-hw3/hw_attempt/app.js
+++ solution-hw3/hw_attempt/app.js
@@ -1,109 +1,182 @@
-// A list of cars with models and descriptions. We will create these
-// using a class in later assignments, but it's not necessary for
-// this simple example.
-let allCars = [
-  {
-    model: '',
-    description: '',
-  },
+/*-------PART 1: CREATING CINNAMON CONSTRUCTOR AND OBJECTS-------*/
 
-];
-
-/**
- * Updates the UI to display a particular car's info.
- * @param car A car object containing a model and a description.
- */
-function displayCar(car) {
-  let carTitleElement = document.querySelector('#car-title');
-  let carInfoElement = document.querySelector('#car-info');
-
-  carTitleElement.innerText = car.model;
-  carInfoElement.innerText = car.description;
+//Constructor function for cinnamon roll objects
+function cinnamonRoll (glazingOptions, glazingPrice) {
+  this.glazingOptions= glazingOptions;
+  this.glazingPrice=glazingPrice;
 }
 
-function onSelectValueChange() {
-  // In this function, `this` corresponds to the select
-  // element. So `this.value` will contain the value of the
-  // selected option as a string.
-  console.log('You selected ' + this.value);
+// Create objects for cinnamon roll glazing options
+const original= new cinnamonRoll ("Keep original", 0.00)
+const sugar= new cinnamonRoll ("Sugar milk", 0.00)
+const vanilla= new cinnamonRoll ("Vanilla milk", 0.50)
+const chocolate= new cinnamonRoll ("Double chocolate", 1.50)
 
-  // We need to convert the string value to an integer
-  let carIndex = parseInt(this.value);
 
-  // Now retrieve the object at the index specified by the select's value
-  let carToDisplay = allCars[carIndex];
+/*-------PART 2: CREATE DYNAMIC DROPDOWN MENU FOR GLAZING OPTIONS-------*/
 
-  // Update the UI
-  displayCar(carToDisplay);
-}
 
 // When the page loads, find the select element.
-let selectElement = document.querySelector('#car-select');
+let selectGlazingOptions = document.querySelector('#glazingOptions');
 
-// Let's add a new car to the allCars array.
-let originalFlavor = 
-{
-  model: 'Keep original',
-  description: '0.00',
-}
-
-allCars.push(originalFlavor);
-
-let sugarFlavor = 
-{
-  model: 'Sugar milk',
-  description: '0.00',
-}
-
-allCars.push(sugarFlavor);
-
-
-let vanillaFlavor = 
-{
-  model: 'Vanilla milk',
-  description: '0.50',
-}
-
-allCars.push(sugarFlavor);
-
-let chocolateFlavor = 
-{
-  model: 'Double chocolate',
-  description: '1.50',
-}
-
-allCars.push(chocolateFlavor);
-
-// We also need to add this new car to the UI. To do that, create a new
-// 'option' HTML element, set its attributes, and add it to the select
-// element.
+// Add original cinnamon roll object to the dropdown menu
 var option = document.createElement('option');
-option.text = originalFlavor.model;
-option.value = allCars.length - 1; // Its value should be the index of the last element in allCars
-selectElement.add(option);
+option.text = original.glazingOptions;
+selectGlazingOptions.add(option);
 
+
+// Add sugar milk cinnamon roll object to the dropdown menu
+var option = document.createElement('option');
+option.text = sugar.glazingOptions;
+selectGlazingOptions.add(option);
+
+// Add vanilla milk cinnamon roll object to the dropdown menu
 
 var option = document.createElement('option');
-option.text = sugarFlavor.model;
-option.value = allCars.length - 1; // Its value should be the index of the last element in allCars
-selectElement.add(option);
+option.text= vanilla.glazingOptions;
+selectGlazingOptions.add(option);
 
+
+//Add double chocolate cinnamon roll object to the dropdown menu
+var option= document.createElement('option');
+option.text= chocolate.glazingOptions;
+selectGlazingOptions.add(option);
+
+
+/*-------PART 3: CREATE CONSTRUCTOR AND OBJECTS FOR PACKING OPTIONS-------*/
+
+//Constructor for pack options
+function packingOptions (packSize, priceAdaption){
+  this.packSize=packSize;
+  this.priceAdaption=priceAdaption;
+}
+
+// Create objects for pack options
+const packOne = new packingOptions (1, 1)
+const packThree = new packingOptions (3, 3)
+const packSix = new packingOptions (6, 5)
+const packTwelve = new packingOptions (12, 10)
+
+
+/*-------PART 4: CREATE DYNAMIC DROPDOWN FOR PACKING OPTIONS-------*/
+
+// When the page loads, find the select element.
+let selectPackingOptions = document.querySelector('#packingOptions');
+
+// Add packOne object to the dropdown menu
+var option = document.createElement('option');
+option.text = packOne.packSize;
+selectPackingOptions.add(option);
+
+
+// Add packThree object to the dropdown menu
+var option = document.createElement('option');
+option.text = packThree.packSize;
+selectPackingOptions.add(option);
+
+// Add packSix object to the dropdown menu
 
 var option = document.createElement('option');
-option.text = vanillaFlavor.model;
-option.value = allCars.length - 1; // Its value should be the index of the last element in allCars
-selectElement.add(option);
+option.text = packSix.packSize;
+selectPackingOptions.add(option);
 
 
+//Add packTwelve object to the dropdown menu
 var option = document.createElement('option');
-option.text = chocolateFlavor.model;
-option.value = allCars.length - 1; // Its value should be the index of the last element in allCars
-selectElement.add(option);
+option.text = packTwelve.packSize;
+selectPackingOptions.add(option);
 
-// Give it a listener for the 'change' event, which is a function that will run
-// when the selected option changes. You could also do this by setting the
-// onchange property of selectElement, e.g. selectElement.onchange = ...
-selectElement.addEventListener('change', onSelectValueChange);
 
-// Initially, display the first car
-displayCar(allCars[0]);
+function glazingChange(element) {
+  // get value of selected glazing option
+  const priceChange = element.value;
+  
+// add your code to do update the price ...
+}
+
+
+
+
+
+
+
+
+
+
+/* Declare arrays for glazing option, glazing price, pack options, and pack size
+
+const glazingPrice = [0.00, 0.00, 0.50, 1.50];
+const packingOptions= [1, 3, 5, 10];
+const packSize= [1, 3, 6, 12];
+const basePrice=2.49
+const glazingOptions = ['Keep original', 'Sugar milk', 'Vanilla milk', 'Chocolate milk'];
+
+//Create objects for each flavor
+const original = {
+  glazingOptions: "Keep original",
+  glazingPrice: 0.00, 
+  basePrice: 2.49,
+  fullName: function (){
+    return this.glazingOptions;
+  }
+}
+
+
+
+document.getElementById("glazingOptions").innerHTML = original.fullName();
+
+/*
+const sugar = {
+  glazingOptions: "Sugar milk",
+  glazingPrice: 0.00, 
+  basePrice: 2.49,
+}
+
+const vanilla = {
+  glazingOptions: "Vanilla milk",
+  glazingPrice: 0.50, 
+  basePrice: 2.49,
+}
+
+const chocolate = {
+  glazingOptions: "Chocolate milk",
+  glazingPrice: 1.50, 
+  basePrice: 2.49,
+}
+*/
+
+/*For Loop calculates total price
+for (i=0; i < glazingOptions.length; i++){
+  if (glazingOptions[i]=='Keep original') {
+  totalPrice= (basePrice[i]+glazingPrice[i]) * packPrice[i];
+}
+  
+  else if (glazingOptions[i] == 'Sugar milk') {
+  totalPrice= (basePrice[i]+glazingPrice[i]) * packPrice[i]; 
+}
+
+  else if (glazingOptions[i] == 'Vanilla milk') {
+  totalPrice= (basePrice[i]+glazingPrice[i]) * packPrice[i];
+}
+
+  else if (glazingOptions[i] == 'Vanilla milk') {
+  totalPrice= (basePrice[i]+glazingPrice[i]) * packPrice[i]}
+}
+*/
+
+
+/*Create dropdown menu for glazing Options*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
