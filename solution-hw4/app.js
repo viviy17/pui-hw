e48ed26652c5fc5cc9f8cd39673a3e7fcb959251
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-09-27 16:19:35 -0400

    update HW04

---
+++ solution-hw4/app.js
@@ -0,0 +1,64 @@
+ // Database from rollsData.js
+ const rolls = {
+  "Original": {
+      "basePrice": 2.49,
+      "imageFile": "original-cinnamon-roll.jpg"
+  },
+  "Apple": {
+      "basePrice": 3.49,
+      "imageFile": "apple-cinnamon-roll.jpg"
+  },
+  "Raisin": {
+      "basePrice": 2.99,
+      "imageFile": "raisin-cinnamon-roll.jpg"
+  },
+  "Walnut": {
+      "basePrice": 3.49,
+      "imageFile": "walnut-cinnamon-roll.jpg"
+  },
+  "Double-Chocolate": {
+      "basePrice": 3.99,
+      "imageFile": "double-chocolate-cinnamon-roll.jpg"
+  },
+  "Strawberry": {
+      "basePrice": 3.99,
+      "imageFile": "strawberry-cinnamon-roll.jpg"
+  }    
+};
+  
+
+//Create an empty array called cart
+
+const cart= { 
+
+}
+
+//Parse the URL parameter and store the current roll type as a variable
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get(‘roll’);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
