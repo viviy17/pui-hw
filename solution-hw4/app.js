46b82e02fc8f359b8daa0ad1461f037140c48b8a
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-09-28 21:41:53 -0400

    HW04 Update

--- solution-hw4/app.js
+++ solution-hw4/app.js
@@ -1,46 +1,79 @@
- // Database from rollsData.js
- const rolls = {
-  "Original": {
-      "basePrice": 2.49,
-      "imageFile": "original-cinnamon-roll.jpg"
-  },
-  "Apple": {
-      "basePrice": 3.49,
-      "imageFile": "apple-cinnamon-roll.jpg"
-  },
-  "Raisin": {
-      "basePrice": 2.99,
-      "imageFile": "raisin-cinnamon-roll.jpg"
-  },
-  "Walnut": {
-      "basePrice": 3.49,
-      "imageFile": "walnut-cinnamon-roll.jpg"
-  },
-  "Double-Chocolate": {
-      "basePrice": 3.99,
-      "imageFile": "double-chocolate-cinnamon-roll.jpg"
-  },
-  "Strawberry": {
-      "basePrice": 3.99,
-      "imageFile": "strawberry-cinnamon-roll.jpg"
-  }    
-};
-  
+ 
+console.log(rolls);
+console.log(rolls["Apple"]);
+console.log(rolls["Apple"].imageFile);
 
-//Create an empty array called cart
 
-const cart= { 
+document.querySelector(".gallery-description").innerText=rollType+" " +"Cinnamon Roll";
+document.querySelector(".border").src=rolls[rollType].imageFile;
+document.querySelector("#totalPrice").innerText="$"+ rolls[rollType].basePrice;
 
-}
 
 //Parse the URL parameter and store the current roll type as a variable
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
-const rollType = params.get(‘roll’);
+const rollType = params.get("roll");
+
+
+//Create function to add cart
+
+function addToCart(){
+    console.log("Function is running");
+    //Create a roll object
+    let roll= newRoll(rollType,"wsfsdfs","sdfsfsfsf",rolls[rollType].basePrice);
+    //add to cart
+    cart.push(roll1);
+    console.log(cart);
+
+}
 
 
 
 
+
+
+//Create dictionaries with key value pairs 
+const glazingOptions = { "Keep original": 0.00, "Sugar milk": 0.00, "Vanilla milk": 0.50, "Double chocolate": 1.50 }
+const packingOptions = { 1: 1, 3: 3, 6: 5, 12: 10 }
+
+//Resource: Stack Overflow https://stackoverflow.com/questions/34913675/how-to-iterate-keys-values-in-javascript
+//Create a for loop to update dropdown menu for glazing options.
+let selectGlazingOptions = document.querySelector('#glazingOptions');
+
+for (const [key, value] of Object.entries(glazingOptions)) {
+  var opt = document.createElement('option');
+  opt.value = [key];
+  opt.innerText = [key];
+  selectGlazingOptions.appendChild(opt);
+}
+
+//Create a for loop to update dropdown menu for packing options.
+let selectPackingOptions = document.querySelector('#packingOptions');
+
+for (const [key, value] of Object.entries(packingOptions)) {
+  var opt = document.createElement('option');
+  opt.value = [key];
+  opt.innerText = [key];
+  selectPackingOptions.appendChild(opt);
+}
+
+//Create function to calculate price.
+function calcPrice() {
+  let selectGlazingOptions = document.querySelector('#glazingOptions');
+  let selectPackingOptions = document.querySelector('#packingOptions');
+  let basePrice=2.49
+  let glazingPrice = glazingOptions[selectGlazingOptions.value];
+  let packSize = packingOptions[selectPackingOptions.value];
+  let finalPrice = (rolls[rollType].basePrice + glazingPrice) * packSize;
+  display(finalPrice);
+}
+
+
+function display(finalPrice) {
+  let selectTotalPrice = document.querySelector("#totalPrice");
+  selectTotalPrice.innerHTML = '$'+ finalPrice.toFixed(2);
+  console.log(selectTotalPrice);
+}
 
 
 
