e4ccf7fe5d91db59fab131ce97d0fdd644612c8c
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-10-07 17:14:02 -0400

    HW05 update

--- solution-hw5/shopping.js
+++ solution-hw5/shopping.js
@@ -1,9 +1,11 @@
 //Parse the URL parameter and store the current roll type as a variable
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
-const rollType = params.get("rolls")
+const rollType = params.get("rolls");
 
-//Create a class
+
+
+//Create a class for rolls
 class Roll {
     constructor(rollType, rollGlazing, packSize, basePrice){
     this.type= rollType;
@@ -19,9 +21,38 @@ const walnutRoll= new Roll("Walnut", "Vanilla Milk", 12, 39.90);
 const raisinRoll= new Roll("Raisin", "Sugar Milk", 3, 8.97);
 const appleRoll= new Roll("Apple", "Original", 3, 10.47);
 
-console.log(originalRoll);
-console.log(walnutRoll);
-console.log(raisinRoll);
-console.log(appleRoll);
+/*
+//Create a set
+const rollSet= new Set();
+
+//Create function to add cinnamon rolls (use "add" in sets, "push" in arrays)
+function addNewRoll (rollType, rollGlazing, packSize, basePrice) { //declare function name and parameters
+  const rollItem = new Roll(rollType, rollGlazing, packSize, basePrice); //declare variable roll item 
+  rollSet.add(rollItem); //add rollItem variable to set "rollSet"
+  return rollItem; //returns rollItem
+}
 
 
+//add a call to function createElement
+for (const rollItem of rollSet){
+  createElement(rollItem);
+}
+
+//add createElement function
+function createElement (rollItem) { 
+  const template = document.querySelector('#roll-template'); //select template ID in HTML file
+  const clone = template.content.cloneNode(true); //copy content inside template using cloneNode method. This does a deep copy, including notecard and child elements.
+  rollItem.element = clone.querySelector('.shopping-container-1');
+  const shoppingContainerElement = document.querySelector('.shopping-container');
+  console.log("createElement function works");
+}
+
+
+//create updateElement function
+function updateElement(rollItem) {
+  const noteImageElement = rollItem.element.querySelector('.shopping-container-1');
+  const noteBodyElement = rollItem.element.querySelector('.shopping-description');
+  noteImageElement.src = rollItem.noteimageURL;
+  noteBodyElement.innerText = rollItem.noteBody;
+}
+*/
