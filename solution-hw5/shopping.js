a54f1b2f47e4295809317c8aba00320a30565cb2
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-10-08 19:29:20 -0400

    HW05 Update

--- solution-hw5/shopping.js
+++ solution-hw5/shopping.js
@@ -6,12 +6,19 @@ class Roll {
     this.size = packSize;
     this.multiple= packMultiple;
     this.basePrice = basePrice;
+    this.element= null;
   }
   
   createElement() {
     const template = document.querySelector('#roll-template'); //select template ID in HTML file
     const clone = template.content.cloneNode(true); //copy content inside template using cloneNode method. This does a deep copy, including notecard and child elements.
     this.element = clone.querySelector('.shopping-container'); //clone div class container shopping-container
+
+    const btnRemove = this.element.querySelector('.remove'); //select garbage icon to delete
+    btnRemove.addEventListener('click', () => { //add event listener with arrow function
+        deleteRoll(this);
+    });
+
   }
 
   updateElement() {
@@ -19,11 +26,12 @@ class Roll {
     const rollFlavorElement = this.element.querySelector('.roll-flavor');
     const rollSizeElement= this.element.querySelector('.roll-size');
     const rollPriceElement=this.element.querySelector('.roll-price')
+    const subTotal=(this.basePrice*this.multiple).toFixed(2);
   
     rollNameElement.innerText = this.type;
     rollFlavorElement.innerText= this.glazing;
     rollSizeElement.innerText= this.size;
-    rollPriceElement.innerText= (this.basePrice*this.multiple).toFixed(2);
+    rollPriceElement.innerText= subTotal;
 
     const rollImgElement = this.element.querySelector('.roll-img')
     rollImgElement.src = 'images/' + this.type.toLowerCase() + '-cinnamon-roll.jpg'
@@ -32,12 +40,12 @@ class Roll {
 }
 
 //Create a set
-const rollSet = new Set();
+const cart = new Set();
 
 //Create function to add cinnamon rolls (use "add" in sets, "push" in arrays)
 function addNewRoll(rollType, rollGlazing, packSize, packMultiple, basePrice) { //declare function name and parameters
   const rollItem = new Roll(rollType, rollGlazing, packSize, packMultiple, basePrice); //declare variable roll item 
-  rollSet.add(rollItem); //add rollItem variable to set "rollSet"
+  cart.add(rollItem); //add rollItem variable to set "cart"
   return rollItem; //returns rollItem
 }
 
@@ -47,29 +55,41 @@ addNewRoll("Walnut", "Vanilla Milk", 12, 10, 3.99);
 addNewRoll("Raisin", "Sugar Milk", 3, 3, 2.99);
 addNewRoll("Apple", "Original", 3, 3, 3.49);
 
-//Declare a variable pulling div class roll container
+//Declare a variable rollContainer pulling div class roll container
 const rollContainer = document.querySelector('.roll-container')
 
 //add a call to function createElement
-for (const rollItem of rollSet) {
+
+let result=0
+
+for (const rollItem of cart) {
   rollItem.createElement();
-  rollItem.updateElement()
-  rollContainer.prepend(rollItem.element);
+  rollItem.updateElement();
+  rollContainer.prepend(rollItem.element); //prepend to rollContainer
+  result=result+(rollItem.multiple*rollItem.basePrice)
+  console.log("roll item multiple is: ", rollItem.multiple)
+  console.log("roll item base price is: ", rollItem.basePrice)
+  console.log("roll subtotal is: ", rollItem.multiple*rollItem.basePrice)
+  console.log(result);
 }
 
+
 //Create a delete function to remove items in cart
-function deleteRoll () {
-  const element= document.querySelector('.shopping-container');
-  element.remove();
+function deleteRoll(rollItem) {
+  // const element= document.querySelector('.shopping-container');
+  // console.log(rollItem);
+  // console.log(rollItem.element);
+  rollItem.element.remove();
+  cart.delete(rollItem);
+  result=result-(rollItem.multiple*rollItem.basePrice);
+  calcPrice()
 }
 
 
 //Create function to calculate price.
 function calcPrice() {
   const selectTotalPrice = document.querySelector(".total-price");
-  selectTotalPrice.innerHTML = "this is the final price";
-
-
+  selectTotalPrice.innerHTML = result.toFixed(2);
 }
 
 
