aff293a52e0ba833383206f87960f5f654eeded2
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-10-08 12:16:45 -0400

    Update HW 05

--- solution-hw5/shopping.js
+++ solution-hw5/shopping.js
@@ -1,55 +1,53 @@
-// //Parse the URL parameter and store the current roll type as a variable
-// const queryString = window.location.search;
-// const params = new URLSearchParams(queryString);
-// const rollType = params.get("rolls");
-
-
-
 //Create a class for rolls
 class Roll {
-  constructor(rollType, rollGlazing, packSize, basePrice) {
+  constructor(rollType, rollGlazing, packSize, packMultiple, basePrice) {
     this.type = rollType;
     this.glazing = rollGlazing;
     this.size = packSize;
+    this.multiple= packMultiple;
     this.basePrice = basePrice;
   }
-
+  
   createElement() {
     const template = document.querySelector('#roll-template'); //select template ID in HTML file
     const clone = template.content.cloneNode(true); //copy content inside template using cloneNode method. This does a deep copy, including notecard and child elements.
-    this.element = clone.querySelector('.shopping-container');
-    const shoppingContainerElement = document.querySelector('.shopping-container');
+    this.element = clone.querySelector('.shopping-container'); //clone div class container shopping-container
   }
 
   updateElement() {
-    // const noteImageElement = this.element.querySelector('.shopping-container-1');
     const rollNameElement = this.element.querySelector('.roll-name');
-    // noteImageElement.src = this.noteimageURL;
+    const rollFlavorElement = this.element.querySelector('.roll-flavor');
+    const rollSizeElement= this.element.querySelector('.roll-size');
+    const rollPriceElement=this.element.querySelector('.roll-price')
+  
     rollNameElement.innerText = this.type;
+    rollFlavorElement.innerText= this.glazing;
+    rollSizeElement.innerText= this.size;
+    rollPriceElement.innerText= (this.basePrice*this.multiple).toFixed(2);
+
     const rollImgElement = this.element.querySelector('.roll-img')
     rollImgElement.src = 'images/' + this.type.toLowerCase() + '-cinnamon-roll.jpg'
   }
+
 }
 
 //Create a set
 const rollSet = new Set();
 
-//Create object for rolls
-addNewRoll("Original", "Sugar Milk", 1, 2.49);
-addNewRoll("Walnut", "Vanilla Milk", 12, 39.90);
-addNewRoll("Raisin", "Sugar Milk", 3, 8.97);
-addNewRoll("Apple", "Original", 3, 10.47);
-
-
-
-
 //Create function to add cinnamon rolls (use "add" in sets, "push" in arrays)
-function addNewRoll(rollType, rollGlazing, packSize, basePrice) { //declare function name and parameters
-  const rollItem = new Roll(rollType, rollGlazing, packSize, basePrice); //declare variable roll item 
+function addNewRoll(rollType, rollGlazing, packSize, packMultiple, basePrice) { //declare function name and parameters
+  const rollItem = new Roll(rollType, rollGlazing, packSize, packMultiple, basePrice); //declare variable roll item 
   rollSet.add(rollItem); //add rollItem variable to set "rollSet"
   return rollItem; //returns rollItem
 }
 
+//Create object for rolls
+addNewRoll("Original", "Sugar Milk", 1, 1, 2.49);
+addNewRoll("Walnut", "Vanilla Milk", 12, 10, 3.99);
+addNewRoll("Raisin", "Sugar Milk", 3, 3, 2.99);
+addNewRoll("Apple", "Original", 3, 3, 3.49);
+
+//Declare a variable pulling div class roll container
 const rollContainer = document.querySelector('.roll-container')
 
 //add a call to function createElement
@@ -59,4 +57,27 @@ for (const rollItem of rollSet) {
   rollContainer.prepend(rollItem.element);
 }
 
+//Create a delete function to remove items in cart
+function deleteRoll () {
+  const element= document.querySelector('.shopping-container');
+  element.remove();
+}
+
+
+//Create function to calculate price.
+function calcPrice() {
+  let selectGlazingOptions = document.querySelector('#glazingOptions');
+  let selectPackingOptions = document.querySelector('#packingOptions');
+  let glazingPrice = glazingOptions[selectGlazingOptions.value];
+  let packSize = packingOptions[selectPackingOptions.value];
+  let finalPrice = 
+  display(finalPrice);
+}
+
+
+function display(finalPrice) {
+  let selectTotalPrice = document.querySelector("#totalPrice");
+  selectTotalPrice.innerHTML = '$'+ finalPrice.toFixed(2);
+  console.log(selectTotalPrice);
+}
 
