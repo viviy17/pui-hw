b0ada9e8407e4324de8a69dd60617f6e79a40552
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-10-08 10:21:32 -0400

    Update HW 05
    Update HW05

--- solution-hw5/shopping.js
+++ solution-hw5/shopping.js
@@ -1,58 +1,62 @@
-//Parse the URL parameter and store the current roll type as a variable
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get("rolls");
+// //Parse the URL parameter and store the current roll type as a variable
+// const queryString = window.location.search;
+// const params = new URLSearchParams(queryString);
+// const rollType = params.get("rolls");
 
 
 
 //Create a class for rolls
 class Roll {
-    constructor(rollType, rollGlazing, packSize, basePrice){
-    this.type= rollType;
-    this.glazing= rollGlazing;
-    this.size= packSize;
-    this.basePrice= basePrice;
-    }
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+    this.type = rollType;
+    this.glazing = rollGlazing;
+    this.size = packSize;
+    this.basePrice = basePrice;
   }
 
-//Create object for rolls
-const originalRoll= new Roll("Original", "Sugar Milk", 1, 2.49);
-const walnutRoll= new Roll("Walnut", "Vanilla Milk", 12, 39.90);
-const raisinRoll= new Roll("Raisin", "Sugar Milk", 3, 8.97);
-const appleRoll= new Roll("Apple", "Original", 3, 10.47);
+  createElement() {
+    const template = document.querySelector('#roll-template'); //select template ID in HTML file
+    const clone = template.content.cloneNode(true); //copy content inside template using cloneNode method. This does a deep copy, including notecard and child elements.
+    this.element = clone.querySelector('.shopping-container');
+    const shoppingContainerElement = document.querySelector('.shopping-container');
+  }
 
-/*
+  updateElement() {
+    // const noteImageElement = this.element.querySelector('.shopping-container-1');
+    const rollNameElement = this.element.querySelector('.roll-name');
+    // noteImageElement.src = this.noteimageURL;
+    rollNameElement.innerText = this.type;
+    const rollImgElement = this.element.querySelector('.roll-img')
+    rollImgElement.src = 'images/' + this.type.toLowerCase() + '-cinnamon-roll.jpg'
+  }
+}
+
 //Create a set
-const rollSet= new Set();
+const rollSet = new Set();
+
+//Create object for rolls
+addNewRoll("Original", "Sugar Milk", 1, 2.49);
+addNewRoll("Walnut", "Vanilla Milk", 12, 39.90);
+addNewRoll("Raisin", "Sugar Milk", 3, 8.97);
+addNewRoll("Apple", "Original", 3, 10.47);
+
+
+
 
 //Create function to add cinnamon rolls (use "add" in sets, "push" in arrays)
-function addNewRoll (rollType, rollGlazing, packSize, basePrice) { //declare function name and parameters
+function addNewRoll(rollType, rollGlazing, packSize, basePrice) { //declare function name and parameters
   const rollItem = new Roll(rollType, rollGlazing, packSize, basePrice); //declare variable roll item 
   rollSet.add(rollItem); //add rollItem variable to set "rollSet"
   return rollItem; //returns rollItem
 }
 
+const rollContainer = document.querySelector('.roll-container')
 
 //add a call to function createElement
-for (const rollItem of rollSet){
-  createElement(rollItem);
-}
-
-//add createElement function
-function createElement (rollItem) { 
-  const template = document.querySelector('#roll-template'); //select template ID in HTML file
-  const clone = template.content.cloneNode(true); //copy content inside template using cloneNode method. This does a deep copy, including notecard and child elements.
-  rollItem.element = clone.querySelector('.shopping-container-1');
-  const shoppingContainerElement = document.querySelector('.shopping-container');
-  console.log("createElement function works");
+for (const rollItem of rollSet) {
+  rollItem.createElement();
+  rollItem.updateElement()
+  rollContainer.prepend(rollItem.element);
 }
 
 
-//create updateElement function
-function updateElement(rollItem) {
-  const noteImageElement = rollItem.element.querySelector('.shopping-container-1');
-  const noteBodyElement = rollItem.element.querySelector('.shopping-description');
-  noteImageElement.src = rollItem.noteimageURL;
-  noteBodyElement.innerText = rollItem.noteBody;
-}
-*/
