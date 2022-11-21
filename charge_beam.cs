75768e860079a2673b6e6a43b01eedb1865f985c
viviy17 <112287535+viviy17@users.noreply.github.com>
2022-11-21 15:52:46 -0500

    add c# file charge beam

---
+++ charge_beam.cs
@@ -0,0 +1,14 @@
+public void OnAltFireEvent() {
+        Debug.Log("Maji has fired a purification blast."); //this is C# print statement
+        public bool blast = false; //code whether blast will be true
+
+        //if Maji's health < , cannot blast
+        
+        //else she can blast
+
+        
+        
+
+        //when do we want to recharge Maji's blast?
+        
+    }
